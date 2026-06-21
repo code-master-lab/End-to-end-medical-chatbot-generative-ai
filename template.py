26f5219f5af81600db80806be671185aadab8e38
Vivek <rautlata59@gmail.com>
2026-06-21 23:34:44 +0530

    I Actually add New concept That's why I Modified my template.py file That was a Structure Of my Project folders That contain of my project. That tell What the template He used for this project That's it And I actually modified for the purpose was add Modified Folders and file because of I add a new concept Just because of  that  I am modified

--- template.py
+++ template.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 import logging
-
+ 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
-
+ 
  
 list_of_files = [
     "src/__init__.py",
@@ -12,25 +12,27 @@ list_of_files = [
     ".env",
     "setup.py",
     "app.py",
+    "frontend_part/chat.html",
+    "frontend_part/style.css",
+    "data/.gitkeep",
+    "data2/.gitkeep",
     "research/trials.ipynb",
-   " test.py"
+    "research/ingest_3rd_edition.ipynb",
 ]
-
-
+ 
+ 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-
-
-    if filedir !="":
+ 
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
-
+ 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file: {filepath}")
-
-
+        logging.info(f"Creating empty file: {filepath}")
+ 
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} already exists and has content — skipped.")
